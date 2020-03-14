c98dcdab22aaf75501283a994abbb8d174e8c686
Jens Bodal <jensbodal@gmail.com>
2020-03-13 20:29:36 -0700

    chore: add node -r esm example

--- example-project/test.sh
+++ example-project/test.sh
@@ -1,5 +1,9 @@
 # testing tsc => babel => babel-node
-yarn babelify && yarn test:babel-node
+yarn babelify && \
+  yarn test:babel-node && \
+  yarn test:node-esm
 
 # testing babel => babel-node
-yarn babelify-ts && yarn test:babel-node
+yarn babelify-ts && \
+  yarn test:babel-node && \
+  yarn test:node-esm
