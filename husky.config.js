696fbbfcf5ce0c3facc1837012e0578b2661b12a
Jens Bodal <jensbodal@gmail.com>
2020-03-18 00:43:31 -0700

    chore: add commitlint and dummy commit message template
    Also add git-commit-template husky plugin

--- husky.config.js
+++ husky.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   hooks: {
+    'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': 'lint-staged',
+    'prepare-commit-msg': 'git-commit-template .commit-template',
   },
 };
